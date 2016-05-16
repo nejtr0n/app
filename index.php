160dd72a1a924a3756f97198c56bd7f01e6cda45
nejtr0n <a6y@xakep.ru>
2016-05-16 14:18:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-<?php
-echo $_SERVER["FRONT"].'.'.gethostname().'.'.$_SERVER["PHY"].'.'.$_SERVER["DC"];
+<html>
+  <head><link rel="icon" href="data:;base64,iVBORw0KGgo="></head>
+  <body>
+    <?php
+    echo $_SERVER["FRONT"].'.'.gethostname().'.'.$_SERVER["PHY"].'.'.$_SERVER["DC"];
+    ?>
+  </body>
+</html>
